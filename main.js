611cd3f358d79f6e04f07772241a19f1ee9875ce
Divi Newton <divinewton@gmail.com>
2024-11-26 12:30:28 -0800

    completed meaning page

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ function openMeaning() {
     document.getElementById("moment-section").style.display = "none";
     document.getElementById("meaning-link").style.textDecoration = "underline";
     document.getElementById("moment-link").style.textDecoration = "none";
+    window.scrollTo(0, 0);
 }
 function openMoment() {
     document.getElementById("moment-section").style.display = "flex";
     document.getElementById("meaning-section").style.display = "none";
     document.getElementById("moment-link").style.textDecoration = "underline";
     document.getElementById("meaning-link").style.textDecoration = "none";
+    window.scrollTo(0, 0);
 }
